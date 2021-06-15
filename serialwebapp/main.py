27341a08b31f850a81da143cb3fd0521be3fea5f
Asha <62386554+asha-ndf@users.noreply.github.com>
2021-06-16 08:00:03 +0900

    シリアル通信のCOMポート探索実装

--- serialwebapp/main.py
+++ serialwebapp/main.py
@@ -1,9 +1,32 @@
+#Config
+import serial
 from flask import Flask
 app = Flask(__name__)
+bitRate = 9600
 
+#Serial
+ser = serial.Serial()
+portlist = []
+for i in range(10):
+    portlist.append('COM'+str(i))
+def openport():
+    global ser
+    if ser.is_open:
+        ret =1
+        pass
+    else:
+        for i in portlist:
+            try:
+                ser = serial.Serial(i, bitRate, timeout=0.1)
+                ret= 1
+                break
+            except:
+                ret = 0
+    return ret
+#Web App
 @app.route('/')
 def status():
-    return 'This is status page'
+    return str(openport())
 
 @app.route('/open')
 def dopen():
